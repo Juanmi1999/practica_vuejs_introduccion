9ca04384914f902c363428b5e6771094e7d97056
Juan Miguel Perez Sanchez <juanmi.ps0@gmail.com>
2022-02-04 17:11:05 +0100

    Practica 1

--- js/script.js
+++ js/script.js
@@ -43,6 +43,8 @@ var server_data = {
 var app = new Vue({
   el: '#app',
   data: {
-    col: server_data
+    col: server_data,
+    title:server_data.collection.title,
+    arrayItems:server_data.collection.items,
   }
 });
